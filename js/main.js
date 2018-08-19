bb44ebf41a1b73bd9b1e8f75401d68114a11c60c
Taras <taras.prezlyata.sign.in@gmail.com>
2018-08-19 18:31:52 +0300

    validation

--- js/main.js
+++ js/main.js
@@ -8,16 +8,31 @@
 	}
 }());
 
-// Function Back to Top
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 200) {
-	  $('.top_btn').fadeIn(200);
-		} else {
-			$('.top_btn').fadeOut(200);
-	   }
-	});   
-	  // Animate the scroll to top
-	$('.top_btn').click(function(event) {
-	  event.preventDefault();
-	$('html, body').animate({scrollTop: 0}, 300);
+//// Function Back to Top
+//$(window).scroll(function() {
+//  if ($(this).scrollTop() > 200) {
+//	  $('.top_btn').fadeIn(200);
+//		} else {
+//			$('.top_btn').fadeOut(200);
+//	   }
+//	});   
+//	  // Animate the scroll to top
+//	$('.top_btn').click(function(event) {
+//	  event.preventDefault();
+//	$('html, body').animate({scrollTop: 0}, 300);
+//});
+
+// Smooth scrolling
+$(document).ready(function(){
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
 });
